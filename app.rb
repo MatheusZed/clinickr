7d7bc13f2bb699bdcfc03c575a94ee3e0ca5b8ff
Matheus de Oliveira Souza <matheus.oliveira2018@outlook.com.br>
2022-03-31 15:31:31 -0300

    adiciona endpoints com namespaces

---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'sinatra/namespace'
+require 'sinatra/activerecord'
+require './models/test'
+require './models/doctor'
+require './models/patient'
+require './models/exam'
+require 'pry'
+
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  get '/tests/:result_token' do |token|
+    result = Exam.where(result_token: token)
+    json = JSON.parse(
+      result.to_json(
+        methods: %i[patient],
+        only: %i[result_token result_date],
+        include:
+        {
+          doctor:
+          {
+            only: %i[crm crm_state name]
+          },
+          tests:
+          {
+            only: %i[test_type test_limits result]
+          }
+        }
+      )
+    )
+
+    json.to_json
+  end
+
+  get '/patients' do
+    Patient.all.to_json
+  end
+
+  get '/doctors' do
+    Doctor.all.to_json
+  end
+end
